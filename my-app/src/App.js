65c3165d937afd26ddeffc7fad5ab4f198490bb1
Arseny Arnold <kulbakocnt@gmail.com>
2024-02-23 21:24:29 +0300

    basic scturcture

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <p>Placeholder text button</p>
+        <input type = 'checkbox' />
+        <p>Placeholder text button</p>
+        <input type = 'checkbox' />
+        <p>Placeholder text button</p>
+        <input type = 'checkbox' />
+        <p>Placeholder text button</p>
+        <input type = 'checkbox' />
+    </div>
+  );
+}
+
+export default App;
